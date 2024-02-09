655f3627538f02a82aaad9d6dcaf29e4adb219c6
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-02-08 16:00:19 -0800

    Remove the last java.util.function usage.
    This should help RoboVM.

--- src/main/java/com/github/tommyettinger/gand/utils/SearchProcessor.java
+++ src/main/java/com/github/tommyettinger/gand/utils/SearchProcessor.java
@@ -1,11 +1,12 @@
 package com.github.tommyettinger.gand.utils;
 
-import java.util.function.Consumer;
-
 import com.github.tommyettinger.gand.algorithms.SearchStep;
 
-public interface SearchProcessor<V> extends Consumer<SearchStep<V>> {
-
-
-
+/**
+ * Essentially the same as a {@code Consumer<SearchStep<V>>}, this is a functional interface that is typically run by
+ * search algorithms at each step. Its functional method is {@link #accept(Object)}, where accept is given a SearchStep
+ * of type V.
+ * @param <V> the type parameter for each SearchStep; the vertex type
+ */
+public interface SearchProcessor<V> extends ObjConsumer<SearchStep<V>> {
 }
