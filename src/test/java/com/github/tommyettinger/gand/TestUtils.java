87dc6de069672dbfeae6b8963c0df9c65677a9e5
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-02-25 19:07:37 -0800

    Significantly speed up CaveAStarTest.
    This also adds a dependency on cringe so we can use it to produce 3D noise and use that as a basis for a map.

--- src/test/java/com/github/tommyettinger/gand/TestUtils.java
+++ src/test/java/com/github/tommyettinger/gand/TestUtils.java
@@ -23,7 +23,11 @@ SOFTWARE.
  */
 package com.github.tommyettinger.gand;
 
-import com.badlogic.gdx.math.*;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.RandomXS128;
+import com.badlogic.gdx.math.Vector2;
+import com.github.tommyettinger.cringe.ContinuousNoise;
+import com.github.tommyettinger.cringe.FoamNoise;
 import com.github.tommyettinger.gand.points.PointF2;
 import com.github.tommyettinger.gand.points.PointF3;
 import com.github.tommyettinger.gand.points.PointI2;
@@ -40,13 +44,13 @@ class TestUtils {
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (i<n-1) {
-                    Vector2 v1 = new Vector2(i, j), v2 = new Vector2(i+1,j);
+                if (i < n - 1) {
+                    Vector2 v1 = new Vector2(i, j), v2 = new Vector2(i + 1, j);
                     graph.addEdge(v1, v2, v1.dst(v2));
                     if (graph.isDirected()) graph.addEdge(v2, v1, v1.dst(v2));
                 }
-                if (j<n-1) {
-                    Vector2 v1 = new Vector2(i, j), v2 = new Vector2(i,j+1);
+                if (j < n - 1) {
+                    Vector2 v1 = new Vector2(i, j), v2 = new Vector2(i, j + 1);
                     graph.addEdge(v1, v2, v1.dst(v2));
                     if (graph.isDirected()) graph.addEdge(v2, v1, v1.dst(v2));
                 }
@@ -54,6 +58,7 @@ class TestUtils {
         }
         return graph;
     }
+
     static Graph<PointI2> makeGridGraphI2(Graph<PointI2> graph, int n) {
 
         for (int i = 0; i < n; i++) {
@@ -65,13 +70,13 @@ class TestUtils {
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (i<n-1) {
-                    PointI2 v1 = new PointI2(i, j), v2 = new PointI2(i+1,j);
+                if (i < n - 1) {
+                    PointI2 v1 = new PointI2(i, j), v2 = new PointI2(i + 1, j);
                     graph.addEdge(v1, v2, v1.dst(v2));
                     if (graph.isDirected()) graph.addEdge(v2, v1, v1.dst(v2));
                 }
-                if (j<n-1) {
-                    PointI2 v1 = new PointI2(i, j), v2 = new PointI2(i,j+1);
+                if (j < n - 1) {
+                    PointI2 v1 = new PointI2(i, j), v2 = new PointI2(i, j + 1);
                     graph.addEdge(v1, v2, v1.dst(v2));
                     if (graph.isDirected()) graph.addEdge(v2, v1, v1.dst(v2));
                 }
@@ -79,6 +84,7 @@ class TestUtils {
         }
         return graph;
     }
+
     static Graph<PointF2> makeGridGraphF2(Graph<PointF2> graph, int n) {
 
         for (int i = 0; i < n; i++) {
@@ -90,13 +96,13 @@ class TestUtils {
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (i<n-1) {
-                    PointF2 v1 = new PointF2(i, j), v2 = new PointF2(i+1,j);
+                if (i < n - 1) {
+                    PointF2 v1 = new PointF2(i, j), v2 = new PointF2(i + 1, j);
                     graph.addEdge(v1, v2, v1.dst(v2));
                     if (graph.isDirected()) graph.addEdge(v2, v1, v1.dst(v2));
                 }
-                if (j<n-1) {
-                    PointF2 v1 = new PointF2(i, j), v2 = new PointF2(i,j+1);
+                if (j < n - 1) {
+                    PointF2 v1 = new PointF2(i, j), v2 = new PointF2(i, j + 1);
                     graph.addEdge(v1, v2, v1.dst(v2));
                     if (graph.isDirected()) graph.addEdge(v2, v1, v1.dst(v2));
                 }
@@ -104,6 +110,7 @@ class TestUtils {
         }
         return graph;
     }
+
     static Graph<PointF3> makeGridGraphF3(Graph<PointF3> graph, int n) {
 
         long newSeed = MathUtils.random.nextLong();
@@ -126,7 +133,7 @@ class TestUtils {
                 v1.y = j;
                 for (int k = 0; k < n; k++) {
                     v1.z = k;
-                    if(!graph.contains(v1))
+                    if (!graph.contains(v1))
                         continue;
                     if (i < n - 1) {
                         v2.set(i + 1, j, k);
@@ -136,14 +143,14 @@ class TestUtils {
                         }
                     }
                     if (j < n - 1) {
-                        v2.set(i, j+1, k);
+                        v2.set(i, j + 1, k);
                         if (graph.contains(v2)) {
                             graph.addEdge(v1, v2, v1.dst(v2));
                             if (graph.isDirected()) graph.addEdge(v2, v1, v1.dst(v2));
                         }
                     }
                     if (k < n - 1) {
-                        v2.set(i, j, k+1);
+                        v2.set(i, j, k + 1);
                         if (graph.contains(v2)) {
                             graph.addEdge(v1, v2, v1.dst(v2));
                             if (graph.isDirected()) graph.addEdge(v2, v1, v1.dst(v2));
@@ -198,48 +205,48 @@ class TestUtils {
             "#.........##.......##...#".toCharArray(),
             "#########################".toCharArray(),
     },
-    DUNGEON2 = {
-            "#########################".toCharArray(),
-            "#........##..##....######".toCharArray(),
-            "#........##..##.....#...#".toCharArray(),
-            "#........#...##.........#".toCharArray(),
-            "#..###..##..............#".toCharArray(),
-            "#....#####..........#...#".toCharArray(),
-            "##...........##.....#..##".toCharArray(),
-            "###..........##....##...#".toCharArray(),
-            "######...############...#".toCharArray(),
-            "######..####....#########".toCharArray(),
-            "###.....####....######..#".toCharArray(),
-            "#.......####....######..#".toCharArray(),
-            "#.....###.##....#.......#".toCharArray(),
-            "#.....#.................#".toCharArray(),
-            "#.....#.................#".toCharArray(),
-            "#.........####..##......#".toCharArray(),
-            "#.##......####..##......#".toCharArray(),
-            "#.##......####..##..##..#".toCharArray(),
-            "##############..######..#".toCharArray(),
-            "##############..######..#".toCharArray(),
-            "##############..######..#".toCharArray(),
-            "##############..######..#".toCharArray(),
-            "####............#####...#".toCharArray(),
-            "#.###...................#".toCharArray(),
-            "#..###..................#".toCharArray(),
-            "#...###......#####....###".toCharArray(),
-            "##...##.....######....###".toCharArray(),
-            "###........##...........#".toCharArray(),
-            "####.....###............#".toCharArray(),
-            "####..######..###########".toCharArray(),
-            "##.....#####......#######".toCharArray(),
-            "##.....#####.....####...#".toCharArray(),
-            "#.........##....#####...#".toCharArray(),
-            "#...##....##..####......#".toCharArray(),
-            "#..#####..##....##.....##".toCharArray(),
-            "#..#####..##........#####".toCharArray(),
-            "#..#####..##........#####".toCharArray(),
-            "#.##......######....#####".toCharArray(),
-            "###.......###.....#######".toCharArray(),
-            "#########################".toCharArray(),
-    };
+            DUNGEON2 = {
+                    "#########################".toCharArray(),
+                    "#........##..##....######".toCharArray(),
+                    "#........##..##.....#...#".toCharArray(),
+                    "#........#...##.........#".toCharArray(),
+                    "#..###..##..............#".toCharArray(),
+                    "#....#####..........#...#".toCharArray(),
+                    "##...........##.....#..##".toCharArray(),
+                    "###..........##....##...#".toCharArray(),
+                    "######...############...#".toCharArray(),
+                    "######..####....#########".toCharArray(),
+                    "###.....####....######..#".toCharArray(),
+                    "#.......####....######..#".toCharArray(),
+                    "#.....###.##....#.......#".toCharArray(),
+                    "#.....#.................#".toCharArray(),
+                    "#.....#.................#".toCharArray(),
+                    "#.........####..##......#".toCharArray(),
+                    "#.##......####..##......#".toCharArray(),
+                    "#.##......####..##..##..#".toCharArray(),
+                    "##############..######..#".toCharArray(),
+                    "##############..######..#".toCharArray(),
+                    "##############..######..#".toCharArray(),
+                    "##############..######..#".toCharArray(),
+                    "####............#####...#".toCharArray(),
+                    "#.###...................#".toCharArray(),
+                    "#..###..................#".toCharArray(),
+                    "#...###......#####....###".toCharArray(),
+                    "##...##.....######....###".toCharArray(),
+                    "###........##...........#".toCharArray(),
+                    "####.....###............#".toCharArray(),
+                    "####..######..###########".toCharArray(),
+                    "##.....#####......#######".toCharArray(),
+                    "##.....#####.....####...#".toCharArray(),
+                    "#.........##....#####...#".toCharArray(),
+                    "#...##....##..####......#".toCharArray(),
+                    "#..#####..##....##.....##".toCharArray(),
+                    "#..#####..##........#####".toCharArray(),
+                    "#..#####..##........#####".toCharArray(),
+                    "#.##......######....#####".toCharArray(),
+                    "###.......###.....#######".toCharArray(),
+                    "#########################".toCharArray(),
+            };
 
     public static final char[][][] DUNGEON_3D = {
             DUNGEON,
@@ -250,13 +257,31 @@ class TestUtils {
     static {
         RandomXS128 random = new RandomXS128(123456789);
         char[][] halls = DUNGEON_3D[1];
-        for (int y = 1; y < halls.length-1; y++) {
-            for (int x = 1; x < halls[y].length-1; x++) {
-                if((x & y & 3) == 1 && random.nextFloat() < 0.15f){
+        for (int y = 1; y < halls.length - 1; y++) {
+            for (int x = 1; x < halls[y].length - 1; x++) {
+                if ((x & y & 3) == 1 && random.nextFloat() < 0.15f) {
                     halls[y][x] = '.';
                 } else halls[y][x] = '#';
             }
         }
     }
 
+    public static final float[][] NOISE_2D = new float[125][75];
+
+    public static final float[][][] NOISE_3D = new float[40][40][20];
+    {
+        ContinuousNoise noise = new ContinuousNoise(new FoamNoise(123), 123, 0.05f, ContinuousNoise.FBM, 2);
+        for (int x = 0; x < NOISE_2D.length; x++) {
+            for (int y = 0; y < NOISE_2D[x].length; y++) {
+                NOISE_2D[x][y] = noise.getNoise(x, y);
+            }
+        }
+        for (int x = 0; x < NOISE_3D.length; x++) {
+            for (int y = 0; y < NOISE_3D[x].length; y++) {
+                for (int z = 0; z < NOISE_3D[x][y].length; z++) {
+                    NOISE_3D[x][y][z] = noise.getNoise(x, y, z);
+                }
+            }
+        }
+    }
 }
